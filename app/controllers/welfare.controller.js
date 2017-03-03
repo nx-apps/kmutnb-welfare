4d4d92c8bbc57c4409ffee316d6106dff65e0a27
Muice^^ <moomookingza@gmail.com>
2017-03-03 18:02:23 +0700

    update list

--- app/controllers/welfare.controller.js
+++ app/controllers/welfare.controller.js
@@ -1,55 +1,61 @@
 exports.list = function (req, res) {
     var r = req.r
     r.db('welfare').table('welfare')
-    .merge(function(m){
-        return {
-            start_date : m('start_date').split('T')(0),
-            end_date : m('end_date').split('T')(0)
-        }
-    })
-        .run()
-        .then(function (result) {
-            res.json(result)
-        })
-        .catch(function (err) {
-            res.status(500).json(err)
-        })
-}
-exports.listId = function (req, res) {
-    var r = req.r
-    r.db('welfare').table('welfare')
-        .get(req.params.id)
         .merge(function (wel_merge) {
             return {
                 employees: r.db('welfare').table('employee').coerceTo('array')
+                    .eqJoin('academic_id', r.db('welfare').table('academic')).without({ right: 'id' }).zip()
+                    .eqJoin('active_id', r.db('welfare').table('active')).without({ right: 'id' }).zip()
+                    .eqJoin('department_id', r.db('welfare').table('department')).without({ right: 'id' }).zip()
+                    .eqJoin('faculty_id', r.db('welfare').table('faculty')).without({ right: 'id' }).zip()
+                    .eqJoin('gender_id', r.db('welfare').table('gender')).without({ right: 'id' }).zip()
+                    .eqJoin('matier_id', r.db('welfare').table('matier')).without({ right: 'id' }).zip()
+                    .eqJoin('position_id', r.db('welfare').table('position')).without({ right: 'id' }).zip()
+                    .eqJoin('prefixname_id', r.db('welfare').table('prefixname')).without({ right: 'id' }).zip()
+                    .eqJoin('type_employee_id', r.db('welfare').table('type_employee')).without({ right: 'id' }).zip()
+                    .without('academic_id', 'active_id', 'department_id', 'faculty_id', 'gender_id', 'matier_id', 'position_id', 'prefixname_id', 'type_employee_id')
             }
         })
         .merge(function (wel_merge) {
             return {
                 countCon: wel_merge('condition').count(),
-                employee1: wel_merge('condition').map(function (con_map) {
-                    return wel_merge('employees').filter(function (f) {
-                        return f(con_map('field')).do(function (d) {
-                            return r.branch(con_map('logic').eq(">="),
-                                d.ge(con_map('value')),
-                                r.branch(con_map('logic').eq(">"),
-                                    d.gt(con_map('value')),
-                                    r.branch(con_map('logic').eq("<="),
-                                        d.le(con_map('value')),
-                                        r.branch(con_map('logic').eq("<"),
-                                            d.lt(con_map('value')),
-                                            r.branch(con_map('logic').eq("="),
-                                                d.eq(con_map('value')),
-                                                d.ne(con_map('value'))
+                // condition1: wel_merge('condition')
+                //     .merge(function (m) {
+                //         return {
+                //             value: m('logic').do(function (d) {
+                //                 return r.branch(d.eq('>'),
+                //                     r.epochTime(r.ISO8601(wel_merge('end_date')).sub(r.ISO8601(m('value')))).toISO8601()
+                //                     , m('value')
+                //                 )
+                //             })
+                //         }
+                //     }),
+                employee1: r.branch(wel_merge('condition').count().eq(0),
+                    [wel_merge('employees')],
+                    wel_merge('condition').map(function (con_map) {
+                        return wel_merge('employees').filter(function (f) {
+                            return f(con_map('field')).do(function (d) {
+                                return r.branch(con_map('logic').eq(">="),
+                                    d.ge(con_map('value')),
+                                    r.branch(con_map('logic').eq(">"),
+                                        d.gt(con_map('value')),
+                                        r.branch(con_map('logic').eq("<="),
+                                            d.le(con_map('value')),
+                                            r.branch(con_map('logic').eq("<"),
+                                                d.lt(con_map('value')),
+                                                r.branch(con_map('logic').eq("=").or(con_map('logic').eq("==")),
+                                                    d.eq(con_map('value')),
+                                                    d.ne(con_map('value'))
+                                                )
                                             )
                                         )
                                     )
                                 )
-                            )
+                            })
                         })
+                            .coerceTo('array')
                     })
-                        .coerceTo('array')
-                })
+                )
             }
         })
         .merge(function (wel_merge) {
@@ -73,7 +79,111 @@ exports.listId = function (req, res) {
         .merge(function (wel_merge) {
             return {
                 employee: wel_merge('employee3')
-                    .filter({ count: wel_merge('countCon') })
+                    .filter(function (emp_filter) {
+                        return r.branch(wel_merge('countCon').eq(0),
+                            emp_filter('count').eq(wel_merge('countCon').add(1)),
+                            emp_filter('count').eq(wel_merge('countCon'))
+                        )
+                    })
+                    // .filter({ count: wel_merge('countCon') })
+                    .coerceTo('array')
+                    .distinct()
+                    .without('count')
+            }
+        })
+        .without('employees', 'employee1', 'employee2', 'employee3', 'countCon')
+        .merge(function (m) {
+            return {
+                start_date: m('start_date').split('T')(0),
+                end_date: m('end_date').split('T')(0)
+            }
+        })
+        .run()
+        .then(function (result) {
+            res.json(result)
+        })
+        .catch(function (err) {
+            res.status(500).json(err)
+        })
+}
+exports.listId = function (req, res) {
+    var r = req.r
+    r.db('welfare').table('welfare')
+        .get(req.params.id)
+        .merge(function (wel_merge) {
+            return {
+                employees: r.db('welfare').table('employee').coerceTo('array')
+                    .eqJoin('academic_id', r.db('welfare').table('academic')).without({ right: 'id' }).zip()
+                    .eqJoin('active_id', r.db('welfare').table('active')).without({ right: 'id' }).zip()
+                    .eqJoin('department_id', r.db('welfare').table('department')).without({ right: 'id' }).zip()
+                    .eqJoin('faculty_id', r.db('welfare').table('faculty')).without({ right: 'id' }).zip()
+                    .eqJoin('gender_id', r.db('welfare').table('gender')).without({ right: 'id' }).zip()
+                    .eqJoin('matier_id', r.db('welfare').table('matier')).without({ right: 'id' }).zip()
+                    .eqJoin('position_id', r.db('welfare').table('position')).without({ right: 'id' }).zip()
+                    .eqJoin('prefixname_id', r.db('welfare').table('prefixname')).without({ right: 'id' }).zip()
+                    .eqJoin('type_employee_id', r.db('welfare').table('type_employee')).without({ right: 'id' }).zip()
+                    .without('academic_id', 'active_id', 'department_id', 'faculty_id', 'gender_id', 'matier_id', 'position_id', 'prefixname_id', 'type_employee_id')
+            }
+        })
+        .merge(function (wel_merge) {
+            return {
+                countCon: wel_merge('condition').count(),
+                employee1: r.branch(wel_merge('condition').count().eq(0),
+                    [wel_merge('employees')],
+                    wel_merge('condition').map(function (con_map) {
+                        return wel_merge('employees').filter(function (f) {
+                            return f(con_map('field')).do(function (d) {
+                                return r.branch(con_map('logic').eq(">="),
+                                    d.ge(con_map('value')),
+                                    r.branch(con_map('logic').eq(">"),
+                                        d.gt(con_map('value')),
+                                        r.branch(con_map('logic').eq("<="),
+                                            d.le(con_map('value')),
+                                            r.branch(con_map('logic').eq("<"),
+                                                d.lt(con_map('value')),
+                                                r.branch(con_map('logic').eq("=").or(con_map('logic').eq("==")),
+                                                    d.eq(con_map('value')),
+                                                    d.ne(con_map('value'))
+                                                )
+                                            )
+                                        )
+                                    )
+                                )
+                            })
+                        })
+                            .coerceTo('array')
+                    })
+                )
+            }
+        })
+        .merge(function (wel_merge) {
+            return {
+                employee2: wel_merge('employee1').reduce(function (l, r) {
+                    return l.add(r)
+                })
+            }
+        })
+        .merge(function (wel_merge) {
+            return {
+                employee3: wel_merge('employee2').merge(function (emp2_merge) {
+                    return {
+                        count: wel_merge('employee2').filter(function (f) {
+                            return f('id').eq(emp2_merge('id'))
+                        }).count()
+                    }
+                })
+            }
+        })
+        .merge(function (wel_merge) {
+            return {
+                employee: wel_merge('employee3')
+                    .filter(function (emp_filter) {
+                        return r.branch(wel_merge('countCon').eq(0),
+                            emp_filter('count').eq(wel_merge('countCon').add(1)),
+                            emp_filter('count').eq(wel_merge('countCon'))
+                        )
+                    })
+                    // .filter({ count: wel_merge('countCon') })
                     .coerceTo('array')
                     .distinct()
                     .without('count')
@@ -82,7 +192,7 @@ exports.listId = function (req, res) {
         .without('employees', 'employee1', 'employee2', 'employee3', 'countCon')
         .run()
         .then(function (result) {
-            res.json(result)
+            res.json([result])
         })
         .catch(function (err) {
             res.status(500).json(err)
@@ -93,7 +203,11 @@ exports.insert = function (req, res) {
     var valid = req.ajv.validate('welfare', req.body);
     var result = { result: false, message: null, id: null };
     if (valid) {
-        req.body = Object.assign(req.body, { year: req.body.year - 543 });
+        req.body = Object.assign(req.body,
+            {
+                year: req.body.year - 543
+            }
+        );
         r.db('welfare').table('welfare').insert(req.body)
             .run()
             .then((response) => {
@@ -116,7 +230,7 @@ exports.insert = function (req, res) {
 exports.update = function (req, res) {
     var r = req.r;
     // console.log(req.body)
-    req.body = Object.assign(req.body, { year: req.body.year - 543 });
+    req.body = Object.assign(req.body);
     r.db('welfare').table('welfare')
         .get(req.body.id)
         .update(req.body)
--- public/src/redux-store/reducer/welfare.js
+++ public/src/redux-store/reducer/welfare.js
@@ -3,7 +3,8 @@ import { commonAction } from '../config'
 
 const initialState = {
     list: [],
-    select: {}
+    select: {},
+    list_id: []
 }
 
 export function welfareReducer(state = initialState, action) {
@@ -11,6 +12,10 @@ export function welfareReducer(state = initialState, action) {
     switch (action.type) {
         case 'WELFARE_LIST':
             return Object.assign({}, state, { list: action.payload });
+        case 'WELFARE_LIST_ID':
+            return Object.assign({}, state, { list_id: action.payload });
+        case 'CLEAR_DATA':
+            return Object.assign({}, state, { select: {} });
         default:
             return state
     }
@@ -24,7 +29,18 @@ export function welfareAction(store) {
         WELFARE_LIST: function () {
             axios.get('/welfare')
                 .then(function (result) {
+                    // console.log(result.data);
                     store.dispatch({ type: 'WELFARE_LIST', payload: result.data })
+                })
+                .catch(err => {
+
+                })
+        },
+        WELFARE_LIST_ID: function (data) {
+            axios.get('/welfare/' + data)
+                .then(function (result) {
+                    // console.log(result);
+                    store.dispatch({ type: 'WELFARE_LIST_ID', payload: result.data })
                 })
                 .catch(err => {
 
@@ -33,7 +49,7 @@ export function welfareAction(store) {
         WELFARE_INSERT: function (data) {
             // console.log(data);
             var datas = {
-                bugget: parseFloat(data.bugget),
+                budget: parseFloat(data.budget),
                 year: parseInt(data.year),
                 condition: data.condition,
                 start_date: new Date(data.start_date).toISOString(),
@@ -47,8 +63,9 @@ export function welfareAction(store) {
                     console.log(result);
                     this.WELFARE_LIST();
                     this.fire('toast', {
-                        status: 'success', text: 'บันทึกสำเร็จ', callback: function () {
+                        status: 'success', text: 'บันทึกสำเร็จ', callback: ()=> {
                             console.log('success');
+                            this.clearData();
                         }
                     });
                 })
@@ -63,7 +80,7 @@ export function welfareAction(store) {
                     console.log(result);
                     this.WELFARE_LIST();
                     this.fire('toast', {
-                        status: 'success', text: 'ลบสำเร็จ', callback: ()=> {
+                        status: 'success', text: 'ลบสำเร็จ', callback: () => {
                             console.log('success');
                             this.$$('panel-right').close();
                         }
@@ -76,28 +93,32 @@ export function welfareAction(store) {
         WELFARE_EDIT: function (data) {
             // console.log(data);
             var datas = {
-                bugget: parseFloat(data.bugget),
+                id: data.id,
+                budget: parseFloat(data.budget),
                 year: parseInt(data.year),
                 condition: data.condition,
                 start_date: new Date(data.start_date).toISOString(),
                 end_date: new Date(data.end_date).toISOString(),
                 name: data.name
             }
-            console.log(datas);
-            // this.fire('toast', { status: 'load' });
-            // axios.post(`./welfare/insert`, datas)
-            //     .then((result) => {
-            //         console.log(result);
-            //         this.WELFARE_LIST();
-            //         this.fire('toast', {
-            //             status: 'success', text: 'บันทึกสำเร็จ', callback: function () {
-            //                 console.log('success');
-            //             }
-            //         });
-            //     })
-            //     .catch((err) => {
-            //         console.log(err);
-            //     })
+            // console.log(datas);
+            this.fire('toast', { status: 'load' });
+            axios.put(`./welfare/update`, datas)
+                .then((result) => {
+                    console.log(result);
+                    this.WELFARE_LIST();
+                    this.fire('toast', {
+                        status: 'success', text: 'บันทึกสำเร็จ', callback: function () {
+                            console.log('success');
+                        }
+                    });
+                })
+                .catch((err) => {
+                    console.log(err);
+                })
+        },
+        CLEAR_DATA: function () {
+            store.dispatch({ type: 'CLEAR_DATA' });
         }
     }
     ]
