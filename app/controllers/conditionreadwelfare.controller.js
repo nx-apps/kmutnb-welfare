c5686d079691bfcdaacc5eb60aeff28405c86a3f
tonsai-j <tonsai.j@nexts-corp.com>
2017-03-07 09:47:50 +0700

    update list data read welfare

--- app/controllers/conditionreadwelfare.controller.js
+++ app/controllers/conditionreadwelfare.controller.js
@@ -6,12 +6,6 @@ exports.read = function(req,res){
 //   console.log('>>>>>>>',sha1);
     var r = req.r;
     r.db('welfare').table('condition_2')
-    //     .merge(function(f){
-    //     return {
-    //       start_work_date:f('start_work_date').split('T')(0)
-    //     }
-    //   })
-
         .run()
         .then(function (result) {
             res.json(result);
@@ -83,13 +77,22 @@ exports.conditions = function(req,res) {
             data:r.db('welfare').table(f('data_source'))
                 .merge((data_source)=>{
                    return{
-                    name: 1
+                    name: data_source.hasFields('department_name').eq(true).branch(data_source('department_name'),
+                               data_source.hasFields('faculty_name').eq(true).branch(data_source('faculty_name'),
+                  						 data_source.hasFields('gender_name').eq(true).branch(data_source('gender_name'),
+                                data_source.hasFields('academic_name').eq(true).branch(data_source('academic_name'),
+                                  data_source.hasFields('matier_name').eq(true).branch(data_source('matier_name'),
+                                   data_source.hasFields('active_name').eq(true).branch(data_source('active_name'),
+                                 data_source.hasFields('position_name').eq(true).branch(data_source('position_name'),
+                                 data_source.hasFields('relation_name').eq(true).branch(data_source('relation_name'),
+                                  data_source.hasFields('prefixname').eq(true).branch(data_source('prefixname'),
+                                '1')))))))))
                    } 
                 })
+             .without('department_name','faculty_name','academic_name','matier_name','active_name','position_name','relation_name','relation_name','prefixname','gender_name')
             .coerceTo('array')
             }
         })
-
         .run()
         .then(function (result) {
             res.json(result);
