6dcf3fa11ab951ab7d0c72a8068d6e1c875eecf4
Muice^^ <moomookingza@gmail.com>
2017-03-08 15:35:37 +0700

    up

--- public/src/redux-store/index.js
+++ public/src/redux-store/index.js
@@ -5,23 +5,21 @@ import {dispatchActionBehavior} from './config'
 import {commonSystemReducer,commonSystemAction} from './reducer/commonSystem'
 import {authReducer,authAction} from './reducer/auth'
 import {providerReducer,providerAction} from './reducer/provider'
-import {welfareReducer,welfareAction} from './reducer/welfare'
+import {userWelfareReducer,userWelfareAction} from './reducer/userWelfare'
 import {listWelfareReducer,listWelfareAction} from './reducer/listWelfare'
+import {welfareReducer,welfareAction} from './reducer/welfare'
 import {commonDataReducer,commonDataAction} from './reducer/commonData'
 import {usersReducer,usersAction} from './reducer/users'
-import {conditionReadWelfareReducer,conditionReadWelfareAction} from './reducer/conditionReadWelfare'
-
 const rootReducer = combineReducers({
     commonSystem:commonSystemReducer,
     auth:authReducer,
     provider:providerReducer,
-    welfare:welfareReducer,
+    userWelfare:userWelfareReducer,
     listWelfare:listWelfareReducer,
+    welfare:welfareReducer,
     commonData:commonDataReducer,
-    users:usersReducer,
-    conditionReadWelfare:conditionReadWelfareReducer
+    users:usersReducer
 });
-
 const storeApp = createStore(
     rootReducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
@@ -33,8 +31,8 @@ window.dispatchActionBehavior = dispatchActionBehavior();
 window.commonSystemAction = commonSystemAction(storeApp);
 window.authAction = authAction(storeApp);
 window.providerAction = providerAction(storeApp);
-window.welfareAction = welfareAction(storeApp);
+window.userWelfareAction = userWelfareAction(storeApp);
 window.listWelfareAction = listWelfareAction(storeApp);
+window.welfareAction = welfareAction(storeApp);
 window.commonDataAction = commonDataAction(storeApp); 
 window.usersAction = usersAction(storeApp);
-window.conditionReadWelfareAction = conditionReadWelfareAction(storeApp);
