b4185322edd8343f6586c9cb2a5d227eedcee5f5
Muice^^ <moomookingza@gmail.com>
2017-03-10 18:57:45 +0700

    update

---
+++ app/routes/upload.routes.js
@@ -0,0 +1,10 @@
+module.exports = function (app) {
+
+    var upload = require('../controllers/upload.controller');
+    app.route('/file/:emp_id').post(upload.uploadFile);
+    app.route('/file/:id').delete(upload.deleteFile);
+    app.route('/file/:id').get(upload.downloadFile);
+    app.route('/list/:refPath/:emp_id').get(upload.listFilePath);
+    app.route('/list/:emp_id').get(upload.listFileDelete);
+    app.route('/update/:file_id').put(upload.recoveryFile);
+}
