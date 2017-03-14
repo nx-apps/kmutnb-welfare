0af9414d98583a234c12fc1680d3d32f87c338d6
tonsai-j <tonsai.j@nexts-corp.com>
2017-03-14 15:02:30 +0700

    update upload

--- app/routes/employee.routes.js
+++ app/routes/employee.routes.js
@@ -1,5 +1,6 @@
 module.exports = function (app) {
     var controller = require('../controllers/employee/employee.controller');
+    var upload = require('../controllers/employee/upload.controller');
     app.get('/list', controller.list);
     app.post('/insert', controller.insert);
     app.put('/update', controller.update);
@@ -9,6 +10,10 @@ module.exports = function (app) {
     app.get('/:id/welfares/year/:year', controller.welfaresYear);
     app.post('/request/welfare/', controller.requestWelfare);
     app.put('/update/welfare/', controller.updateWelfare);
+
+    // upload
+    
+    app.route('/:emp_id/upload/').post(upload.uploadFile);
 
     // app.delete('/use_welfare/delete/id/:id', controller.deleteWelfare);
 
