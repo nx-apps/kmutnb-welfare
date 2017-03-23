0b77531b135ac4f7173ddb7f975683653f2ba7d1
Muice^^ <moomookingza@gmail.com>
2017-03-23 17:23:16 +0700

    update

--- app/routes/group/fund.routes.js
+++ app/routes/group/fund.routes.js
@@ -1,7 +1,7 @@
 module.exports = function (app) {
     var controller = require('../../controllers/group/fund.controller');
     app.get('/year/:year', controller.list);
-    app.get('/:id', controller.listId);
+    app.get('/id/:id', controller.listId);
     app.post('/insert', controller.insert);
     app.put('/update', controller.update);
     app.delete('/delete/id/:id', controller.delete);
