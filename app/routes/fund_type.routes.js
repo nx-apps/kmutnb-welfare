50d4fc713667b3909b8cc76c3c13f3d187cbfd8c
Muice^^ <moomookingza@gmail.com>
2017-03-24 12:12:17 +0700

    update controller

--- app/routes/fund_type.routes.js
+++ app/routes/fund_type.routes.js
@@ -1,7 +1,6 @@
 module.exports = function (app) {
-    var controller = require('../controllers/fund.controller');
+    var controller = require('../controllers/fund_type.controller');
     app.get('/', controller.list);
-    app.get('/id/:id', controller.listId);
     app.post('/insert', controller.insert);
     app.put('/update', controller.update);
     app.delete('/delete/id/:id', controller.delete);
