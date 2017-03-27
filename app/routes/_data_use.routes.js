765399df05fe3769cfe47e4d6a16be550ffce6d7
Tawatchai Wisutarporn <tawatchai.w@baanprogram.com>
2017-03-27 16:32:41 +0700

    remove files'rent use.

--- app/routes/_data_use.routes.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(app){
-    var controller = require('../controllers/data_use.controller');
-
-    app.get('/', controller.list)
-    // app.get('/:id', controller.Id)
-    app.get('/test', controller.test)
-}
--- app/routes/_user_welfare.routes.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (app) {
-    var controller = require('../controllers/user_welfare.controller');
-    // app.get('/', controller.list);
-    // app.get('/id/:id', controller.listId); //welfare.route
-    // app.get('/groupYear', controller.groupYear); //group/welfare.router
-    // app.get('/groupByYear/year/:year', controller.groupByYear); //group/welfare.route
-    // app.get('/adminEmployee/:welId', controller.adminEmployee); //group/welfare.route
-    // app.get('/welfaresEmployee/:id', controller.welfaresEmployee); //employee.route
-}
