8f8f6b117504297554d5040a15a4f1a576cebb1a
Kitz-Parnupat <dainokung2535@gmail.com>
2017-03-29 14:58:19 +0700

    update rvd

--- public/src/redux-store/reducer/fundRvd.js
+++ public/src/redux-store/reducer/fundRvd.js
@@ -2,7 +2,8 @@
  import { commonAction } from '../config'
  const initialState = {
      data:{},
-     dataList: []
+     dataList: [],
+     userList:[]
  }
  export function fundRvdReducer(state = initialState, action) {
       switch (action.type) {
@@ -12,6 +13,8 @@
             return Object.assign({},state,{data:action.payload});
         case 'FUND_RVD_CLEAR_DATA':
             return Object.assign({},state,{data:{}});
+        case 'FUND_RVD_GET_USER_LIST':
+            return Object.assign({},state,{userList:action.payload});
         default:
             return state
     }
@@ -84,6 +87,88 @@
             },
             FUND_RVD_CLEAR_DATA:function(data){
                 store.dispatch({ type: 'FUND_RVD_CLEAR_DATA' })
+            },
+            FUND_RVD_GET_USER_LIST:function(){
+                axios.get('./rvd/signup/list')
+                .then((response)=>{
+                    console.log(response.data);
+                    var newData = response.data;
+                    newData.map((item)=>{
+                        if (item.academic_name == "") {
+                            item.fullName = item.prefix_name + " " + item.firstname + " " + item.lastname
+                        }
+                        else {
+                            item.fullName = item.academic_name + " " + item.firstname + " " + item.lastname
+                        }
+                        item.check = false;
+                        return item;
+                    })
+                    store.dispatch({type:'FUND_RVD_GET_USER_LIST',payload:newData})
+                })
+                .catch((error)=>{
+                    console.log('error');
+                    console.log(error);
+                });
+            },
+            FUND_RVD_UPDATE_USER:function(id){
+                this.fire('toast',{status:'load'});
+                axios.put('./rvd/signup/update',id)
+                .then((response)=>{
+                   this.fire('toast',{status:'success',text:'บันทึกสำเร็จ',
+                     callback:()=>{
+                          this.FUND_RVD_GET_USER_LIST()
+                     }
+                    });
+                })
+                .catch((error)=>{
+                    console.log('error');
+                    console.log(error);
+                });
+            },
+            FUND_RVD_DELETE_USER:function(id){
+                this.fire('toast',{status:'load'});
+                axios.delete('./rvd/signup/delete/id/'+id)
+                .then((response)=>{
+                    this.fire('toast',{status:'success',text:'บันทึกสำเร็จ',
+                      callback:()=>{
+                          this.FUND_RVD_GET_USER_LIST()
+                      }
+                     });
+                })
+                .catch((error)=>{
+                    console.log('error');
+                    console.log(error);
+                });
+            },
+            FUND_RVD_EXIT_FUND:function(data){
+                this.fire('toast',{status:'load'});
+                axios.put('./rvd/signup/leave/',data)
+                .then((response)=>{
+                    this.fire('toast',{status:'success',text:'บันทึกสำเร็จ',
+                      callback:()=>{
+                          this.FUND_RVD_GET_USER_LIST()
+                      }
+                     });
+                })
+                .catch((error)=>{
+                console.log('error');
+                console.log(error);
+                });
+            },
+            FUND_RVD_EXIT_WORK:function(data){
+                this.fire('toast',{status:'load'});
+                axios.put('rvd/signup/fund/out',data)
+                .then((response)=>{
+                     this.fire('toast',{status:'success',text:'บันทึกสำเร็จ',
+                      callback:()=>{
+                          this.FUND_RVD_GET_USER_LIST()
+                      }
+                     });
+                })
+                .catch((error)=>{
+                    console.log('error');
+                    console.log(error);
+                });
             }
         }
     ]
