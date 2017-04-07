aee2ea76290c56294827e7d4f68c915157c3d9a8
somchit chanudom <somchit.c@nexts-corp.com>
2017-04-07 17:22:21 +0700

    update oauth

--- public/src/redux-store/reducer/auth.js
+++ public/src/redux-store/reducer/auth.js
@@ -1,61 +1,63 @@
 import axios from '../axios'
-import {commonAction} from '../config'
+import { commonAction } from '../config'
 import jwtDecode from 'jwt-decode'
 
 const initialState = {
-    user:{role:'none'}
+    user: { role: 'none' }
 }
 
-export function authReducer(state = initialState,action){
+export function authReducer(state = initialState, action) {
 
     switch (action.type) {
         case 'AUTH_SET_USER':
             var userInfo;
-            if(action.payload.token){
+            if (action.payload.token) {
                 userInfo = jwtDecode(action.payload.token)
-            }else{
+            } else {
                 userInfo = action.payload;
             }
-            return Object.assign({},state,{user:userInfo});
+            return Object.assign({}, state, { user: userInfo });
+        case 'AUTH_INFO':
+            return Object.assign({}, state, { user: action.payload });
         default:
             return state
     }
 
 }
 
-export function authAction(store){
+export function authAction(store) {
 
     return [commonAction(),
-        {
-            AUTH_LOGIN:function(formLogin){
-                axios.post('./auth/login',{username:formLogin.user,password:formLogin.pass})
-                .then((response)=>{
+    {
+        AUTH_LOGIN: function (formLogin) {
+            axios.post('./auth/login', { username: formLogin.user, password: formLogin.pass })
+                .then((response) => {
 
 
-                    localStorage.setItem("token",response.data.token);
-                    store.dispatch({type:'AUTH_SET_USER',payload:response.data})
+                    localStorage.setItem("token", response.data.token);
+                    store.dispatch({ type: 'AUTH_SET_USER', payload: response.data })
 
                     let userInfo;
-                    if(response.data.token){
+                    if (response.data.token) {
                         userInfo = jwtDecode(response.data.token)
-                    }else{
+                    } else {
                         userInfo = response.data;
                     }
 
-                  //  if(userInfo.role=="teacher"){
-                   //     this.fire('nylon-change-page',{path:'/examRoom'})
-                   // }else{
-                   //     this.fire('nylon-change-page',{path:'/examHistory'})
-                   // }
+                    //  if(userInfo.role=="teacher"){
+                    //     this.fire('nylon-change-page',{path:'/examRoom'})
+                    // }else{
+                    //     this.fire('nylon-change-page',{path:'/examHistory'})
+                    // }
 
                 })
-                .catch((error)=>{
+                .catch((error) => {
                     //console.log('error');
-                    console.log({error});
+                    console.log({ error });
                     alert('error')
                 });
-            }
         }
+    }
     ]
 
 }
