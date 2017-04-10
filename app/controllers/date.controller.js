0de7e261852bf6378241f6fd9a56278ac944cde8
tonsai-j <tonsai.j@nexts-corp.com>
2017-04-10 18:01:34 +0700

    update day

--- app/controllers/date.controller.js
+++ app/controllers/date.controller.js
@@ -1,8 +1,13 @@
 exports.currentdate = function (req, res) {
    let  result = new Date()
+   let day =  1000*60 * 60 * 24;
+   let _7day_old = 7*day
+   let now = +new Date(result)
+//    console.log(((+new Date(result))-_7day_old).toISOString())
 //    var date = 
    let date = {
        result: result,
+       dateOld_7: (new Date(now-_7day_old)).toISOString(),
        dateNow : +new Date(result),
        date : result.toISOString(),
        dates : result.toISOString().split('T')[0],
@@ -10,5 +15,6 @@ exports.currentdate = function (req, res) {
        month : result.toISOString().split('T')[0].split('-')[1],
        year : result.toISOString().split('T')[0].split('-')[0],
    }
+
    res.json(date);
 }
