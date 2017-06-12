c8196b7a9e3264ebe6291174d65f0fab6890c468
Tonsai jompun <tonsai.j@nexts-corp.com>
2017-06-12 14:16:16 +0700

    update style vaadin

--- public/src/redux-store/reducer/retier.js
+++ public/src/redux-store/reducer/retier.js
@@ -16,6 +16,7 @@ export function retierAction(store) {
     {
         RETIER_SEARCH: function (data) {
             var tz = "T00:00:00+07:00";
+            this.fire('toast',{status:'load'});
             axios.get('/retier/list/date/' + data+tz)
                 .then((result) => {
                     // console.log(result.data);
