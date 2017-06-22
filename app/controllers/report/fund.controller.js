c7c5acee65708c14b55688d18cea2235e07386fa
somchit chanudom <somchit.c@nexts-corp.com>
2017-06-22 13:04:25 +0700

    update report fund

--- app/controllers/report/fund.controller.js
+++ app/controllers/report/fund.controller.js
@@ -58,44 +58,126 @@ function keysToUpper(param) {
 }
 
 exports.fund01 = function (req, res) {
+    // var r = req.r;
+    // var param = req.query;
+    // param.monthName = "";
+    // if (param.year !== undefined)
+    //     param.year = Number(param.year)
+    // if (param.month !== undefined) {
+    //     param.month = Number(param.month)
+    //     param.monthName = arr_month[param.month]
+    // }
+
+    // r.db('welfare').table('history_fund')//.getAll(param.req.year, { index: 'year' })
+    //     // .filter({ 'personal_id': param.personal_id })
+    //     // // .getAll(param.personal_id, { index: 'personal_id' })
+    //     .filter({
+    //         fund_year: param.year, fund_month: param.month
+    //     })
+    //     .orderBy('fund_code')
+    //     .run()
+    //     .then(function (result) {
+    //         // res.json(result);
+    //         param = keysToUpper(param);
+    //         CURRENT_DATE = new Date().toISOString().slice(0, 10)
+    //         param.CURRENT_DATE = CURRENT_DATE
+    //         res.ireport("fund01.jasper", req.query.EXPORT || req.query.export || "pdf", result, param);
+
     var r = req.r;
     var param = req.query;
+    param.monthName = "";
     if (param.year !== undefined)
         param.year = Number(param.year)
-    r.db('welfare').table('history_fund')//.getAll(param.req.year, { index: 'year' })
-        // .filter({ 'personal_id': param.personal_id })
-        // // .getAll(param.personal_id, { index: 'personal_id' })
-        .filter({
-            fund_year: param.year
-        })
-        .orderBy('fund_code')
+    if (param.month !== undefined) {
+        param.month = Number(param.month)
+        param.monthName = arr_month[param.month]
+    }
+    // console.log(param)
+    var data = r.db('welfare').table('history_fund');//;
+    if (param.policy_code === undefined && param.month === undefined) {
+        data = data.getAll(param.year, { index: 'fund_year' })
+    } else if (param.policy_code !== undefined && param.month === undefined) {
+        data = data.getAll([param.year, param.policy_code], { index: 'yearPolicy' })
+    } else if (param.policy_code !== undefined && param.month !== undefined) {
+        data = data.getAll([param.year, param.month, param.policy_code], { index: 'yearMonthPolicy' })
+    } else if (param.policy_code === undefined && param.month !== undefined) {
+        data = data.getAll([param.year, param.month], { index: 'yearMonth' })
+    }
+    r.expr({
+        data: data.group('personal_id').ungroup()//.count()
+            .coerceTo('array') //.limit(20)//.orderBy('fund_code')
+            // .filter({ policy_code : param.policy_code,
+            //          fund_year: param.year,
+            //          fund_month : param.month
+            //  })
+            // .getAll([param.year, param.month], { index: 'yearMonth' })
+            .merge(function (m2) {
+                return {
+                    com_con: m2('reduction').sum('com_con'),
+                    com_ear: m2('reduction').sum('com_ear'),
+                    emp_con: m2('reduction').sum('emp_con'),
+                    emp_ear: m2('reduction').sum('emp_ear'),
+                    emp_name: m2('reduction')('emp_name')(0),
+                    policy_code: m2('reduction')('policy_code')(0),
+                    total: m2('reduction').sum('total'),
+                    fund_name: m2('reduction')('fund_name')(0),
+                    fund_company: m2('reduction')('fund_company')(0),
+                    fund_uname: m2('reduction')('fund_uname')(0),
+                }
+            })
+
+    }).merge(function (m) {
+        return {
+            params: r.branch(m('data').count().gt(0),
+                m('data')(0).pluck('fund_name', 'fund_company', 'fund_uname'),
+                {}
+            )
+        }
+    })
+
         .run()
-        .then(function (result) {
-            // res.json(result);
-            param = keysToUpper(param);
+        .then(function (data) {
+            // // var param = data['params'];
+            // var result = data['data'];
+            // res.json(data);
+            data['params'] = Object.assign(param, data['params']);
+            data['params'] = keysToUpper(data['params']);
             CURRENT_DATE = new Date().toISOString().slice(0, 10)
             param.CURRENT_DATE = CURRENT_DATE
-            res.ireport("fund01.jasper", req.query.EXPORT || req.query.export || "pdf", result, param);
+            // res.json(data['params']);
+            res.ireport("fund01.jasper", req.query.EXPORT || req.query.export || "pdf", data['data'], data['params']);
         });
 }
 exports.fund02 = function (req, res) {
     var r = req.r;
     var param = req.query;
+    param.monthName = "";
     if (param.year !== undefined)
         param.year = Number(param.year)
-    r.db('welfare').table('history_fund')//.getAll(param.req.year, { index: 'year' })
-        .filter({ 'personal_id': param.personal_id })
-        // // .getAll(param.personal_id, { index: 'personal_id' })
-        .filter({
-            fund_year: param.year
-        })
-        .orderBy('date_updated')
+    if (param.month !== undefined) {
+        param.month = Number(param.month)
+        param.monthName = arr_month[param.month]
+    }
+    r.expr({
+        data: r.db('welfare').table('history_fund')
+            .getAll([param.year, param.personal_id], { index: 'yearPID' })
+            .coerceTo('array')
+    }).merge(function (m) {
+        return {
+            params: r.branch(m('data').count().gt(0),
+                m('data')(0).pluck('fund_name', 'fund_company', 'fund_uname', 'emp_name'),
+                {}
+            )
+        }
+    })
         .run()
-        .then(function (result) {
-            // res.json(result);
-            param = keysToUpper(param);
-            CURRENT_DATE = new Date().toISOString().slice(0, 10)
-            param.CURRENT_DATE = CURRENT_DATE
-            res.ireport("fund02.jasper", req.query.EXPORT || req.query.export || "pdf", result, param);
-        });
+        .then(function (data) {
+            // // var param = data['params'];
+            // var result = data['data'];
+            // res.json(data);
+            data['params'] = Object.assign(param, data['params']);
+            data['params'] = keysToUpper(data['params']);
+            // res.json(data['params']);
+            res.ireport("fund02.jasper", req.query.EXPORT || req.query.export || "pdf", data['data'], data['params']);
+        })
 }
