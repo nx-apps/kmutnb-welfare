cba31312c26aa0d1040a3ae404d960dddc17e1dd
Tonsai jompun <tonsai.j@nexts-corp.com>
2017-06-27 09:52:43 +0700

    update gen excel sso

--- app/routes/sso.routes.js
+++ app/routes/sso.routes.js
@@ -1,6 +1,7 @@
 module.exports = function (app) {
     var controller = require('../controllers/sso.controller');
     app.post('/upload', controller.upload);
+    app.get('/genfile',controller.genSso);
     app.get('/download/id/:id', controller.downloadFile);
     app.get('/getfile/name/:name/sheet/:sheet', controller.getfile);
     app.post('/insert', controller.insert)
