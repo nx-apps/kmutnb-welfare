d6f276aadbd74e0c3c639186d85fbcee4fd9e430
Tonsai jompun <tonsai.j@nexts-corp.com>
2017-06-27 13:35:37 +0700

    update

--- app/controllers/rvd.controller.js
+++ app/controllers/rvd.controller.js
@@ -1,19 +1,3 @@
-exports.list = function (req, res) {
-    var r = req.r
-    r.db('welfare').table('rvd')
-        .run()
-        .then(function (result) {
-            res.json(result);
-        })
-}
-exports.listId = function (req, res) {
-    var r = req.r
-    r.db('welfare').table('rvd').get(req.params.id)
-        .run()
-        .then(function (result) {
-            res.json(result);
-        })
-}
 exports.genFund = function (req, res) {
     var r = req.r
     r.db('welfare').table('employee').getAll('WORK', { index: "active_id" })
@@ -60,67 +44,5 @@ exports.genFund = function (req, res) {
             res.setHeader('Content-Disposition', 'attachment; filename=' + filename);
             res.type('application/octet-stream');
             res.send(wbout);
-        })
-}
-exports.insert = function (req, res) {
-    var r = req.r
-    var valid = req.ajv.validate('rvd', req.body);
-    var result = { result: false, message: null, id: null };
-    if (valid) {
-        req.body = Object.assign(req.body,
-            {
-                date_create: new Date().toISOString(),
-                date_update: new Date().toISOString()
-            }
-        );
-        r.db('welfare').table('rvd').insert(req.body)
-            .run()
-            .then((response) => {
-                result.message = response;
-                if (response.errors == 0) {
-                    result.result = true;
-                    result.id = response.generated_keys;
-                }
-                res.json(result);
-            })
-            .error((err) => {
-                result.message = err;
-                res.json(result);
-            })
-    }
-    else {
-        result.message = req.ajv.errorsText()
-        res.json(result);
-    }
-}
-exports.update = function (req, res) {
-    var r = req.r;
-    req.body = Object.assign(req.body,
-        {
-            date_update: new Date().toISOString()
-        }
-    );
-    r.db('welfare').table('rvd')
-        .get(req.body.id)
-        .update(req.body)
-        .run()
-        .then(function (result) {
-            res.json(result);
-        })
-        .catch(function (err) {
-            res.status(500).json(err);
-        })
-}
-exports.delete = function (req, res) {
-    var r = req.r;
-    r.db('welfare').table('rvd')
-        .get(req.params.id)
-        .delete()
-        .run()
-        .then(function (result) {
-            res.json(result);
-        })
-        .catch(function (err) {
-            res.status(500).json(err);
         })
 }
--- app/routes/rvd.routes.js
+++ app/routes/rvd.routes.js
@@ -1,9 +1,9 @@
 module.exports = function (app) {
     var controller = require('../controllers/rvd.controller');
-    app.get('/', controller.list);
+    // app.get('/', controller.list);
     app.get('/genfile',controller.genFund);
-    app.get('/id/:id', controller.listId);
-    app.post('/insert', controller.insert);
-    app.put('/update', controller.update);
-    app.delete('/delete/id/:id', controller.delete);
+    // app.get('/id/:id', controller.listId);
+    // app.post('/insert', controller.insert);
+    // app.put('/update', controller.update);
+    // app.delete('/delete/id/:id', controller.delete);
 }
